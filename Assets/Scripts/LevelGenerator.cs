d631f4e18bfd31293921f8089c1084d52e565ba7
Julian Currie <julian.currie@gmail.com>
2017-10-08 20:56:15 +0700

    Added functions and config for procedurally platforms

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -37,9 +37,118 @@ public class LevelGenerator : MonoBehaviour
 
   private float platformLastPosX;
 
-  // Use this for initialization
+  private enum PlatformType { None, Flat }
+
+  private class PlatformPositionInfo
+  {
+    public PlatformType platformType;
+    public float postionY;
+    public bool hasMonster;
+    public bool hasHealthItem;
+
+    public PlatformPositionInfo(PlatformType type, float postionY, bool hasMonster, bool hasHealthItem)
+    {
+      this.platformType = type;
+      this.postionY = postionY;
+      this.hasMonster = hasMonster;
+      this.hasHealthItem = hasHealthItem;
+    }
+  }
+
   void Start()
   {
+    GenerateLevel();
+  }
 
+  void FillPositionInfo(PlatformPositionInfo[] platformInfo)
+  {
+    int currentPlatformInfoIndex = 0;
+
+    //First Platform section
+    for (int i = 0; i < startPlatformLength; i++)
+    {
+      platformInfo[currentPlatformInfoIndex].platformType = PlatformType.Flat;
+      platformInfo[currentPlatformInfoIndex].postionY = 0f;
+      currentPlatformInfoIndex++;
+    }
+
+    //All other platform sections before end Platform section
+    while (currentPlatformInfoIndex < levelLength - endPlatformLength)
+    {
+
+      //Creates gaps between platform groups. If the previous index is Flat, ie at end of
+      //start group, we skip one and then create a new platform.
+      //Note: Individual platforms in a group can have various Y positions
+      if (platformInfo[currentPlatformInfoIndex - 1].platformType != PlatformType.None)
+      {
+        currentPlatformInfoIndex++;
+        continue;
+      }
+
+      float platformPositionY = Random.Range(platformPosMinY, platformPosMaxY);
+
+      int platformLength = Random.Range(platformLengthMin, platformLengthMax);
+
+      for (int i = 0; i < platformLength; i++)
+      {
+        bool monsterExists = (Random.Range(0f, 1f) < chanceMonsterExists);
+        bool healthItemExists = (Random.Range(0f, 1f) < chanceItemExists);
+
+        platformInfo[currentPlatformInfoIndex].platformType = PlatformType.Flat;
+        platformInfo[currentPlatformInfoIndex].postionY = platformPositionY;
+        platformInfo[currentPlatformInfoIndex].hasMonster = monsterExists;
+        platformInfo[currentPlatformInfoIndex].hasHealthItem = healthItemExists;
+
+        currentPlatformInfoIndex++;
+
+        //If we touch the beginning of the End platforms
+        if (currentPlatformInfoIndex > (levelLength - endPlatformLength))
+        {
+          currentPlatformInfoIndex = levelLength - endPlatformLength;
+          break;
+        }
+      }
+
+      //End platforms
+      for (int i = 0; i < endPlatformLength; i++)
+      {
+        platformInfo[currentPlatformInfoIndex].platformType = PlatformType.Flat;
+        platformInfo[currentPlatformInfoIndex].postionY = 0f;
+
+        currentPlatformInfoIndex++;
+      }
+    }
+  }
+
+  void CreatePlatformsFromPositionInfo(PlatformPositionInfo[] platformPositionInfo)
+  {
+    for (int i = 0; i < platformPositionInfo.Length; i++)
+    {
+      PlatformPositionInfo positionInfo = platformPositionInfo[i];
+
+      if (positionInfo.platformType == PlatformType.None)
+      {
+        continue;
+      }
+
+      Vector3 platformPosition = new Vector3(distBetweenPlatforms * i, positionInfo.postionY, 0);
+
+      //Group platforms under one GameObject in the Unity editor for clarity
+      Transform createBlock = (Transform)Instantiate(platformPrefab, platformPosition, Quaternion.identity);
+      createBlock.parent = platformParent;
+    }
+  }
+
+  public void GenerateLevel()
+  {
+    PlatformPositionInfo[] platformInfo = new PlatformPositionInfo[levelLength];
+
+    for (int i = 0; i < platformInfo.Length; i++)
+    {
+      platformInfo[i] = new PlatformPositionInfo(PlatformType.None, -1, false, false);
+    }
+
+    FillPositionInfo(platformInfo);
+    CreatePlatformsFromPositionInfo(platformInfo);
   }
 }
