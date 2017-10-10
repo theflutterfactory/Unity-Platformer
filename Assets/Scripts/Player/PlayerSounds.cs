0c627311857b49daf8987c79c7b63089ca14bf9f
Julian Currie <julian.currie@gmail.com>
2017-10-10 20:30:10 +0700

    Added generic and metal foot sounds to animation frames. added PlayerSounds script

---
+++ Assets/Scripts/Player/PlayerSounds.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSounds : MonoBehaviour
+{
+
+  private float collisionSoundEffect = 1f;
+
+  public float audioFootVolume = 1f;
+  public float soundEffectPitch = 0.05f;
+
+  private AudioSource audioSource;
+  public AudioClip genericFootSound;
+  public AudioClip metalFootSound;
+
+  void Awake()
+  {
+    audioSource = GetComponent<AudioSource>();
+  }
+
+  void FootSound()
+  {
+    audioSource.volume = collisionSoundEffect * audioFootVolume;
+    audioSource.pitch = Random.Range(1.0f - soundEffectPitch, 1.0f + soundEffectPitch);
+
+    if (Random.Range(0, 2) > 0)
+    {
+      audioSource.clip = genericFootSound;
+    }
+    else
+    {
+      audioSource.clip = metalFootSound;
+    }
+
+    audioSource.Play();
+  }
+
+}
